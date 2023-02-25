2a2a7d92149d99aafd6bc6ddab5f8816f9799a63
MGage911 <gageem911@gmail.com>
2023-02-25 16:50:21 -0500

    Subclass off of CommandBase not IntantCommand
    LowerIntakeCommand and RaiseIntakeCommand now subclass CommandBase.

--- src/main/java/frc/robot/commands/LowerIntakeCommand.java
+++ src/main/java/frc/robot/commands/LowerIntakeCommand.java
@@ -1,13 +1,13 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import frc.robot.subsystems.IntakeArmsSubsystem;
 
 // This command lowers Ampy's intake arms.
 
-public class LowerIntakeCommand extends InstantCommand {
+public class LowerIntakeCommand extends CommandBase {
 
     private final IntakeArmsSubsystem intakeArms;
 
--- src/main/java/frc/robot/commands/RaiseIntakeCommand.java
+++ src/main/java/frc/robot/commands/RaiseIntakeCommand.java
@@ -1,13 +1,13 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import frc.robot.subsystems.IntakeArmsSubsystem;
 
 // This command raises Ampy's intake arms
 
-public class RaiseIntakeCommand extends InstantCommand {
+public class RaiseIntakeCommand extends CommandBase {
 
     private final IntakeArmsSubsystem intakeArms;
 
