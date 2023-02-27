285058353bf2c0e6cf30433442dc5546def46286
fishyfishfish55 <l.lacossct@gmail.com>
2023-02-27 18:34:03 -0500

    Make the drivetrain adjustable from the SmartDashboard

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,39 +1,107 @@
-
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 
-// This command establishes the commands necessary for Ampy to drive
-
+/**
+ * Command to drive the robot, using speed and rotation values. Requires the
+ * DrivetrainSubsystem.
+ */
 public class DriveCommand extends CommandBase {
+    // Speed values
+    private DoubleSupplier speed;
+    // Value to scale joystick speed input by. Should probably be less than 1.
+    private double speedScale = 1;
+    // SlewRateLimiter and values to smooth acceleration
+    double positiveSpeedRateLimit = 5.0;
+    double negativeSpeedRateLimit = -5.0;
+    private SlewRateLimiter speedLimiter = new SlewRateLimiter(positiveSpeedRateLimit, negativeSpeedRateLimit, 0);
 
-    private final DriveSubsystem drive;
-    private final DoubleSupplier speed;
-    private final DoubleSupplier rotation;
+    // Rotation values
+    private DoubleSupplier turn;
+    // Value to scale joystick rotation input by. Should probably be less than 1.
+    private double turnScale = 0.5;
+    // SlewRateLimiter and values to smooth acceleration
+    double positiveTurnRateLimit = 10.0;
+    double negativeTurnRateLimit = -10.0;
+    private SlewRateLimiter turnLimiter = new SlewRateLimiter(positiveTurnRateLimit, negativeTurnRateLimit, 0);
 
-    public DriveCommand(DoubleSupplier speed, DoubleSupplier rotation, DriveSubsystem drive) {
+    // Controls whether inputs are squared.
+    private boolean squareInputs = false;
 
+    private DriveSubsystem drivetrainSubsystem;
+
+    /**
+     * Command to drive the robot, using speed and rotation values.
+     * 
+     * @param speed               Joystick speed in [-1.0, 1.0]
+     * @param turn                Joystick rotation in [-1.0, 1.0]
+     * @param drivetrainSubsystem The DrivetrainSubsystem
+     */
+    public DriveCommand(DoubleSupplier speed, DoubleSupplier turn, DriveSubsystem drivetrainSubsystem) {
         this.speed = speed;
-        this.rotation = rotation;
-
-        this.drive = drive;
-        addRequirements(drive);
-
+        this.turn = turn;
+        this.drivetrainSubsystem = drivetrainSubsystem;
+        addRequirements(drivetrainSubsystem);
     }
 
     @Override
     public void initialize() {
+        drivetrainSubsystem.stop();
+        SmartDashboard.putNumber("Speed scale", speedScale);
+        SmartDashboard.putNumber("Turn scale", turnScale);
+        SmartDashboard.putBoolean("Square inputs", squareInputs);
+
+        SmartDashboard.putNumber("Positive speed rate limit", positiveSpeedRateLimit);
+        SmartDashboard.putNumber("Negative speed rate limit", negativeSpeedRateLimit);
+        SmartDashboard.putNumber("Positive turn rate limit", positiveTurnRateLimit);
+        SmartDashboard.putNumber("Negative turn rate limit", negativeTurnRateLimit);
+
     }
 
     @Override
     public void execute() {
-        drive.drive(speed.getAsDouble(), rotation.getAsDouble());
+        // Update values from the SmartDashboard
+        speedScale = SmartDashboard.getNumber("Speed scale", speedScale);
+        turnScale = SmartDashboard.getNumber("Turn scale", turnScale);
+        squareInputs = SmartDashboard.getBoolean("Square inputs", squareInputs);
+
+        // Calculate speed values
+        double driveSpeed = speed.getAsDouble() * speedScale * (squareInputs ? Math.abs(speed.getAsDouble()) : 1);
+        double turnSpeed = turn.getAsDouble() * turnScale * (squareInputs ? Math.abs(turn.getAsDouble()) : 1);
+
+        // Update limiters
+        double newPositiveSpeedRateLimit = SmartDashboard.getNumber("Positive speed rate limit",
+                positiveSpeedRateLimit);
+        double newNegativeSpeedRateLimit = SmartDashboard.getNumber("Negative speed rate limit",
+                negativeSpeedRateLimit);
+        double newPositiveTurnRateLimit = SmartDashboard.getNumber("Positive turn rate limit", positiveTurnRateLimit);
+        double newNegativeTurnRateLimit = SmartDashboard.getNumber("Negative turn rate limit", negativeTurnRateLimit);
+
+        if (newPositiveSpeedRateLimit != positiveSpeedRateLimit
+                || newNegativeSpeedRateLimit != negativeSpeedRateLimit) {
+            positiveSpeedRateLimit = newPositiveSpeedRateLimit;
+            negativeSpeedRateLimit = newNegativeSpeedRateLimit;
+            speedLimiter = new SlewRateLimiter(positiveSpeedRateLimit, negativeSpeedRateLimit, driveSpeed);
+        }
+
+        if (newPositiveTurnRateLimit != positiveTurnRateLimit || newNegativeTurnRateLimit != negativeTurnRateLimit) {
+            positiveTurnRateLimit = newPositiveTurnRateLimit;
+            negativeTurnRateLimit = newNegativeTurnRateLimit;
+            turnLimiter = new SlewRateLimiter(positiveTurnRateLimit, negativeTurnRateLimit, turnSpeed);
+        }
+
+        // Drive, applying SlewRateLimiter to smooth out values
+        drivetrainSubsystem.drive(speedLimiter.calculate(driveSpeed), turnLimiter.calculate(turnSpeed));
     }
 
     @Override
     public void end(boolean interrupted) {
+        drivetrainSubsystem.stop();
     }
 
     @Override
@@ -41,10 +109,4 @@ public class DriveCommand extends CommandBase {
         return false;
     }
 
-    @Override
-    public boolean runsWhenDisabled() {
-
-        return false;
-
-    }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,53 +1,96 @@
-
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.DriveSubsystem;
-
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * Subsystem to model the robot's drivetrain
+ */
 public class DriveSubsystem extends SubsystemBase {
-    private WPI_TalonFX leftMaster;
-    private WPI_TalonFX leftFollower;
-    private WPI_TalonFX rightMaster;
-    private WPI_TalonFX rightFollower;
-    private DifferentialDrive drive;
+    // Drive motors
+    WPI_TalonFX leftMaster;
+    WPI_TalonFX leftFollower;
+    WPI_TalonFX rightMaster;
+    WPI_TalonFX rightFollower;
+
+    // DifferentialDrive object for drive calculations
+    DifferentialDrive drive;
 
     public DriveSubsystem() {
-        leftMaster = initMotor("Left Master", 1);
-        leftFollower = initMotor("Left Follower", 2);
-
-        leftMaster.setInverted(TalonFXInvertType.CounterClockwise);
-        leftFollower.setInverted(TalonFXInvertType.FollowMaster);
+        // Motor initialization
+        // Left motors turn clockwise
+        leftMaster = initMotor(1);
+        leftMaster.setInverted(TalonFXInvertType.Clockwise);
+        leftFollower = initMotor(2);
         leftFollower.follow(leftMaster);
+        leftFollower.setInverted(TalonFXInvertType.FollowMaster);
 
-        rightMaster = initMotor("Right Master", 3);
-        rightFollower = initMotor("Right Follower", 4);
-
+        // Right motors turn counterclockwise
+        rightMaster = initMotor(3);
         rightMaster.setInverted(TalonFXInvertType.CounterClockwise);
+        rightFollower = initMotor(4);
+        rightFollower.follow(rightMaster);
         rightFollower.setInverted(TalonFXInvertType.FollowMaster);
-        rightFollower.follow(leftMaster);
 
+        // Drivetrain initialization
         drive = new DifferentialDrive(leftMaster, rightMaster);
-
     }
 
     /**
-     * Drive the robot
-     * 
-     * @param speed    Forward speed in [-1, 1]
-     * @param rotation Curvature in [-1, 1]
+     * Helper method to initialize a WPI_TalonFX.
+     * @param canId The motor's CAN ID
+     * @return newly initialized WPI_TalonFX
      */
-    public void drive(double speed, double rotation) {
-        drive.curvatureDrive(speed, rotation, true);
+    private static WPI_TalonFX initMotor(int canId) {
+        WPI_TalonFX motor = new WPI_TalonFX(canId);
+        motor.configFactoryDefault();
+        return motor;
     }
 
-    private WPI_TalonFX initMotor(String name, int canId) {
-        WPI_TalonFX motor = new WPI_TalonFX(canId);
-        addChild(name, motor);
-        return motor;
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Left master sensor position", leftMaster.getSelectedSensorPosition());
+        SmartDashboard.putNumber("Left master sensor velocity", leftMaster.getSelectedSensorVelocity());
+        SmartDashboard.putNumber("Right master sensor position", rightMaster.getSelectedSensorPosition());
+        SmartDashboard.putNumber("Right master sensor velocity", rightMaster.getSelectedSensorVelocity());
+    }
+
+    /**
+     * Drives the robot
+     * 
+     * @param speed Motor speed as a value in [-1.0, 1.0]
+     * @param turn  The robot's curvature as a value in [-1.0, 1.0]. Also controls
+     *              turn rate for turn-in-place maneuvers
+     */
+    public void drive(double speed, double turn) {
+        drive.curvatureDrive(speed, turn, true);
+    }
+
+    /**
+     * Set the drive motor's neutral mode
+     * A motor's neutral mode determines whether it resists motion - brake mode - or
+     * rotates freely - coast mode - when no power is applied to it.
+     * 
+     * @param neutralMode The motor's neutral mode
+     */
+    public void setNeutralMode(NeutralMode neutralMode) {
+        leftMaster.setNeutralMode(neutralMode);
+        leftFollower.setNeutralMode(neutralMode);
+        rightMaster.setNeutralMode(neutralMode);
+        rightFollower.setNeutralMode(neutralMode);
+    }
+
+    /**
+     * Stop the subsystem
+     */
+    public void stop() {
+        leftMaster.set(0);
+        rightMaster.set(0);
     }
 
 }
