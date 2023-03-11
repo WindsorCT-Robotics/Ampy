b68e0278a6532cda2e2995ed645830538b414302
Norphz <117695842+Norphz@users.noreply.github.com>
2023-03-11 15:07:13 -0500

    final tweaks

---
+++ src/main/java/frc/robot/commands/IntakeFromSubstationCommand.java
@@ -0,0 +1,32 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.ConveyorSubsystem;
+import frc.robot.subsystems.IntakeArmsSubsystem;
+import frc.robot.subsystems.IntakeRollersSubsystem;
+import frc.robot.subsystems.IntakeArmsSubsystem.ArmState;
+
+/**
+ * This command executes multiple commands in sequence in order to intake an
+ * object. It first lowers the intake, then activates the conveyor
+ * and intake rollers at the same time with both of them rotating forwards in
+ * order to intake an object. Afterwards, it raises the intake.
+ */
+public class IntakeFromSubstationCommand extends SequentialCommandGroup {
+        private final double CONVEYOR_SPEED = 0.3;
+        private final double INTAKE_ROLLER_SPEED = -0.3;
+        public IntakeFromSubstationCommand(IntakeArmsSubsystem intakeArms, ConveyorSubsystem conveyor,
+                        IntakeRollersSubsystem intakeRollers) {
+                addCommands(
+                                new MoveIntakeCommand(ArmState.RAISED, intakeArms),
+                                new ParallelCommandGroup(
+                                                new MoveConveyorCommand(CONVEYOR_SPEED, conveyor),
+                                                new MoveIntakeRollersCommand(INTAKE_ROLLER_SPEED, intakeRollers))
+                                                .until(() -> !conveyor.isEmpty()),
+                                new WaitCommand(1),
+                                new MoveConveyorCommand(-CONVEYOR_SPEED, conveyor).withTimeout(0.5));
+        }
+
+}
