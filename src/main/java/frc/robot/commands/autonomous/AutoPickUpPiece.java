5896846dcf4b5cf87dd57301d33c99ea5b9a1704
fishyfishfish55 <l.lacossct@gmail.com>
2023-03-25 16:38:00 -0400

    Polish values

--- src/main/java/frc/robot/commands/autonomous/AutoPickUpPiece.java
+++ src/main/java/frc/robot/commands/autonomous/AutoPickUpPiece.java
@@ -1,14 +1,16 @@
 package frc.robot.commands.autonomous;
 
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.IntakeFromFloorCommand;
 import frc.robot.commands.MoveConveyorCommand;
+import frc.robot.commands.MoveIntakeCommand;
 import frc.robot.subsystems.ConveyorSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeArmsSubsystem;
 import frc.robot.subsystems.IntakeRollersSubsystem;
+import frc.robot.subsystems.IntakeArmsSubsystem.ArmState;
 
 public class AutoPickUpPiece extends SequentialCommandGroup {
     public AutoPickUpPiece(ConveyorSubsystem conveyor, DriveSubsystem drive, IntakeArmsSubsystem intakeArms,
@@ -16,10 +18,11 @@ public class AutoPickUpPiece extends SequentialCommandGroup {
         addCommands(
                 new MoveConveyorCommand(0.8, conveyor).withTimeout(1), // spit out piece
                 new WaitCommand(1), // Wait for cube to settle
-                new ParallelDeadlineGroup(
-                        new AutoDriveCommand(-0.25, 0, drive).withTimeout(4), // go to a piece
+                new ParallelRaceGroup(
+                        new AutoDriveCommand(-0.25, 0, drive).withTimeout(4.25), // go to a piece
                         new IntakeFromFloorCommand(intakeArms, conveyor, intakeRollers)), // pick up the piece
-                new AutoDriveCommand(-0.25, 0, drive).withTimeout(4) // return to the community
+                new MoveIntakeCommand(ArmState.RAISED, intakeArms),
+                new AutoDriveCommand(0.25, 0, drive).withTimeout(4) // return to the community
 
         );
     }
--- src/main/java/frc/robot/commands/autonomous/AutoScoreCommand.java
+++ src/main/java/frc/robot/commands/autonomous/AutoScoreCommand.java
@@ -4,14 +4,12 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.MoveConveyorCommand;
 import frc.robot.subsystems.ConveyorSubsystem;
-import frc.robot.subsystems.DriveSubsystem;
 
 public class AutoScoreCommand extends SequentialCommandGroup {
-    public AutoScoreCommand(ConveyorSubsystem conveyor, DriveSubsystem drive) {
+    public AutoScoreCommand(ConveyorSubsystem conveyor) {
         addCommands(
             new MoveConveyorCommand(0.8, conveyor).withTimeout(1), // spit out piece
-            new WaitCommand(1),
-            new AutoDriveCommand(-0.25, 0, drive) // escape the community
+            new WaitCommand(1)
         );
     }
     
