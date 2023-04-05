8ebfdc673a2662262d858c5b8a794401fe95d052
fishyfishfish55 <l.lacossct@gmail.com>
2023-04-05 15:19:49 -0400

    Remove unused import

--- src/main/java/frc/robot/subsystems/ConveyorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ConveyorSubsystem.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
